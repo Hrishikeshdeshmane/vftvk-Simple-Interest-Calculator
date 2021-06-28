9a06df3d6e438277a6f82b66c0fba5370944d1f8
Hrishikeshdeshmane <71499682+Hrishikeshdeshmane@users.noreply.github.com>
2021-06-28 10:29:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    n = document.getElementById("years").value;
+    r = document.getElementById("rate").value;
+    
+    var a = (p*n*r)/100;
+    
+     document.getElementById("results").innerHTML = a;
     
 }
         
