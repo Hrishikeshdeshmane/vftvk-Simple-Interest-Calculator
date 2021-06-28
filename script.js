a8832d04bb3e9e6cf88afb5e7090178ebfd58b94
Hrishikeshdeshmane <71499682+Hrishikeshdeshmane@users.noreply.github.com>
2021-06-28 15:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function compute()
+<script>
+	function compute()
 {
+    var p,n,r,a;
     p = document.getElementById("principal").value;
     n = document.getElementById("years").value;
     r = document.getElementById("rate").value;
-    
-    var a = (p*n*r)/100;
-     document.getElementById("result").innerHTML = a;
-    
+    a = (p*n*r)/100;
+    var b = Number(p)+ Number(a);
+	var s = Number(n)+2021;
+	
+    document.getElementById("result").innerHTML = a;
+	document.getElementById("amount").innerHTML = b;
+    document.getElementById("pri").innerHTML = p;
+	document.getElementById("time").innerHTML = s;
+	document.getElementById("ra").innerHTML = r;
 }
-        
+	</script>
